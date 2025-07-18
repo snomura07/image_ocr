8e4d446a425a97deeba5bdce5370f5c2285cb787
snomura <you@example.com>
2025-07-19 07:44:06 +0900

    引数を指定するように修正、画像ファイルの存在チェック

--- run.py
+++ run.py
@@ -8,7 +8,15 @@ from utils.PromptGen import PromptGen
 if __name__ == '__main__':
     print("Running the main script...")
 
+    if len(sys.argv) < 2:
+        print("Usage: python DigikeyOcr.py <image_path>")
+        sys.exit(1)
+    image_path = sys.argv[1]
+    if not os.path.isfile(image_path):
+        print(f"Error: File '{image_path}' does not exist.")
+        sys.exit(1)
+
     modOpenAi = ModOpenAi(model="gpt-4o-mini")
     promptGen = PromptGen()
-    res       = modOpenAi.imageOcrRequest(promptGen.genDigikeyImageOcr(), "IMG_3891.JPEG")
+    res       = modOpenAi.imageOcrRequest(promptGen.genDigikeyImageOcr(), image_path)
     print(res.choices[0].message.content)
